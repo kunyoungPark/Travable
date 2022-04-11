78a9b2f5f0d284033a924368dfced459a3d196e9
kunyoungPark <p3145@daum.net>
2022-04-11 19:55:32 +0900

    04/06

--- app/src/main/java/com/kypark/trav_able/Main3.java
+++ app/src/main/java/com/kypark/trav_able/Main3.java
@@ -2,13 +2,17 @@ package com.kypark.trav_able;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class Main3 extends AppCompatActivity {
 
@@ -22,9 +26,15 @@ public class Main3 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String number = "01085043145";
-                if (checkSelfPermission(Main3.this, Manifest.permission.CALL_PHONE)!=Package.M)
-                Intent it = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + number));
-                startActivity(it);
+                if (ContextCompat.checkSelfPermission(Main3.this, Manifest.permission.CALL_PHONE)== PackageManager.PERMISSION_GRANTED) {
+                    Intent it = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + number));
+                    startActivity(it);
+                } else {
+                    ActivityCompat.requestPermissions(
+                            Main3.this,
+                            new String[]{Manifest.permission.CALL_PHONE},
+                            123);
+                }
             }
         });
 
